76bd6834923660f8a3617cf15632772e5b0f9dbc
jrhowell123 <jrhowell@ucsd.edu>
2022-02-04 15:27:15 -0800

    Fixed Tests

--- TestDemoTest.java
+++ TestDemoTest.java
@@ -4,11 +4,11 @@ import org.junit.*;
 public class TestDemoTest {
     @Test
     public void subtraction(){
-        assertEquals(2, TestDemo.subtract(3,3));
+        assertEquals(2, TestDemo.subtract(3,1));//should pass
     }
 
     @Test
     public void multiplication(){
-        assertEquals(6, TestDemo.subtract(3,3));
+        assertEquals(6, TestDemo.subtract(3,2));//should pass
     }
 }
